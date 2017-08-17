92b3c0b4e58fe6831f8b162ca0865cb62ef5427c
mikemwang <mikewang91@gmail.com>
2017-08-17 01:51:40 -0400

    fanned out text does not work, bezier curves look nice

--- sunburst.py
+++ sunburst.py
@@ -7,23 +7,110 @@ from colormap import COLOUR_MAPPING
 import yaml
 
 
+class TextLayer(object):
+    """class to create all the text and format it appropraitely"""
+
+    def __init__(self, text_canvas, radial_offset, angular_spacing):
+        """
+        text_canvas: the canvas object to draw the text
+
+        radial offset: the amount to radially move the text away
+
+        angular spacing: minimum angular distance between letters, degrees?
+        """
+        self.text_canvas = text_canvas
+        self.radial_offset = radial_offset
+        self.angular_spacing = angular_spacing
+        self.sector_info = {}  # info from the sector
+
+    def begin(self):
+        """draw the text!!"""
+        text_info = self.apply_angular_offset()
+        for layer in text_info:
+            cent_rad = text_info[layer]['radius']
+            text_rad = cent_rad+self.radial_offset
+            for angle in text_info[layer]['letters']:
+                cent_angle = angle*pi/180.0
+                text_angle = text_info[layer]['letters'][angle][1]*pi/180.0
+
+                letter = text_info[layer]['letters'][angle][0]
+                dot_x = cent_rad*cos(cent_angle)
+                dot_y = cent_rad*sin(cent_angle)
+                text_x = text_rad*cos(text_angle)
+                text_y = text_rad*sin(text_angle)
+                self.text_canvas.fill(path.circle(dot_x, dot_y, 0.03))
+                self.text_canvas.text(text_x-0.1, text_y, letter)
+                self.text_canvas.stroke(path.line(dot_x, dot_y, text_x,
+                    text_y), [style.linewidth(0.01)])
+
+
+    def update(self, layer, letter, centroid):
+        """sectors will call this to update the text info"""
+        # centroid is a tuple of (angle, radius)
+        if layer not in self.sector_info.keys():
+            self.sector_info[layer] = {'radius': centroid[1],
+                                       'letters':
+                                       {centroid[0]: letter}}
+        else:
+            self.sector_info[layer]['letters'][centroid[0]] = letter
+
+    def apply_angular_offset(self):
+        """apply the angular offset
+        {layer:
+            letters:{
+                a:(centroid_angle, text_angle)
+                b:(centroid_angle, text_angle)
+            },
+            centroid_radius: some_number
+        }
+        """
+        text_info = {}
+        for layer in self.sector_info:
+            ordered_angles = sorted(list(self.sector_info[layer]['letters'].keys()))
+            ordered_new_angles = []
+            text_info[layer] = {'radius': self.sector_info[layer]['radius'],
+                                     'letters': {}}
+            for index in range(len(ordered_angles)):
+                letter = self.sector_info[layer]['letters'][ordered_angles[index]]
+                cur_angle = ordered_angles[index]
+                if index == 0:
+                    text_info[layer]['letters'][cur_angle] = (letter, cur_angle)
+                    ordered_new_angles.append(cur_angle)
+                else:
+                    target_angle = ordered_new_angles[index-1]+self.angular_spacing
+                    if cur_angle < target_angle:
+                        if target_angle - cur_angle < (pi/100.0):
+                            text_info[layer]['letters'][cur_angle] = (letter,
+                                                                   target_angle)
+                            ordered_new_angles.append(target_angle)
+                        else:
+                            ordered_new_angles.append(cur_angle)
+                    else:
+                        text_info[layer]['letters'][cur_angle] = (letter,
+                                                               cur_angle)
+                        ordered_new_angles.append(cur_angle)
+        print("level 1: " + str(len(text_info[1]['letters'])))
+        print("level 2: " + str(len(text_info[2]['letters'])))
+        return text_info
+
+
 class Sunburst(object):
     """An object that describes the entire sunburst diagram, initializes global
     settings"""
 
     def __init__(self,
                  canvases,
-                 config_path):
+                 config_path,
+                 text_object):
 
         self.shape_canvas = canvases[0]
         self.text_canvas = canvases[1]
         # data properties
         self.config_path = config_path  # path to config file
+        self.text_object = text_object
         self.config = None  # to be parsed later
         self.data_attrs = None
         self.layer_attrs = None
-        # stores text locations, so we can do the text spread later
-        self.text_info = {}
 
     def import_settings(self):
         """import settings in the config file and assign them"""
@@ -61,6 +148,9 @@ class Sunburst(object):
                     break
             if not present:
                 sanitized_list.append(sanitized_entry)
+
+            if len(sanitized_list) > 100:
+                break
         return sanitized_list
 
     def bounding_box(self, data_max):
@@ -87,6 +177,7 @@ class Sunburst(object):
         origin = Sector(self, word_list)
         # recursively draw all other layers
         origin.create_child_segments()
+        # self.text_object.begin()
 
 
 class Sector(object):
@@ -103,7 +194,9 @@ class Sector(object):
                  end_percent=1,
                  inner_radius=0,
                  parent_angle=0,
-                 parent_arc=360):
+                 parent_arc=360,
+                 parent_centroid_angle=0,
+                 parent_outer_radius=0):
 
         # assigned by constructor
         self.sunburst = sunburst
@@ -114,6 +207,8 @@ class Sector(object):
         self.end = end_percent  # end where on the parent sector
         # after truncating the parent sector letter
         self.inner_r = inner_radius
+        self.parent_centroid_angle = parent_centroid_angle
+        self.parent_outer_radius = parent_outer_radius
 
         # calculated using constructor
         self.start_angle = self.start*parent_arc + parent_angle
@@ -126,6 +221,8 @@ class Sector(object):
         self.sector_width = self.sunburst.layer_attrs['sector_width']
         self.end_sector_width = self.sunburst.layer_attrs['end_cap_width']
 
+        self.centroid_angle = 0.5*(self.end_angle+self.start_angle)*pi/180.0
+
     def display(self):
         """method to display the sector"""
 
@@ -134,6 +231,12 @@ class Sector(object):
             # don't draw anything in the center
             return
 
+        COLOR = None
+        if self.letter == '':
+            COLOR = [color.rgb.red]
+        else:
+            COLOR = [color.gray(COLOUR_MAPPING[self.letter])]
+
         if self.letter is '':
             segment = path.path(path.arc(0, 0, self.inner_r, self.start_angle,
                                          self.end_angle),
@@ -141,7 +244,7 @@ class Sector(object):
                                           self.end_sector_width+self.inner_r,
                                           self.end_angle, self.start_angle),
                                 path.closepath())
-            self.shape_canvas.fill(segment, [color.rgb.red])
+            self.shape_canvas.fill(segment, COLOR)
         else:
             # create the arc segment
             segment = path.path(path.arc(0, 0, self.inner_r, self.start_angle,
@@ -151,10 +254,24 @@ class Sector(object):
                                           self.end_angle, self.start_angle),
                                 path.closepath())
             # render the arc segment onto the canvas
-            self.shape_canvas.fill(segment,
-                                   [color.gray(COLOUR_MAPPING[self.letter])])
+            self.shape_canvas.fill(segment, COLOR)
 
-            centroid_angle = 0.5*(self.end_angle+self.start_angle)
+
+            #self.sunburst.text_object.update(self.level,
+            #                                 self.letter,
+            #                                 (centroid_angle, centroid_rad))
+
+        x0 = self.inner_r*cos(self.centroid_angle)
+        y0 = self.inner_r*sin(self.centroid_angle)
+        r1 =  0.5*(self.parent_outer_radius+self.inner_r)
+        x1 = r1*cos(self.centroid_angle)
+        y1 = r1*sin(self.centroid_angle)
+        x2 = r1*cos(self.parent_centroid_angle)
+        y2 = r1*sin(self.parent_centroid_angle)
+        x3 = self.parent_outer_radius*cos(self.parent_centroid_angle)
+        y3 = self.parent_outer_radius*sin(self.parent_centroid_angle)
+        self.shape_canvas.stroke(path.curve(x0,y0,x1,y1,x2,y2,x3,y3),
+                                 [style.linewidth(0.005), COLOR[0]])
 
     def create_child_segment_lists(self):
         """a method to create child segments"""
@@ -246,6 +363,8 @@ class Sector(object):
                                   self.inner_r + self.layer_width,
                                   self.start_angle,
                                   self.end_angle - self.start_angle,
+                                  self.centroid_angle,
+                                  self.inner_r+self.sector_width
                                   )
 
             child_sector.create_child_segments()
@@ -256,8 +375,9 @@ if __name__ == "__main__":
     shape_canvas = canvas.canvas()
     text_canvas = canvas.canvas()
     canvases = (shape_canvas, text_canvas)
+    text_object = TextLayer(text_canvas, 0.75, 0.25)
     # create bounding box
-    sunburst = Sunburst(canvases, 'config.yaml')
+    sunburst = Sunburst(canvases, 'config.yaml', text_object)
     sunburst.begin()
     shape_canvas.insert(text_canvas)
     shape_canvas.writePDFfile('test1')
